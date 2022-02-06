d31f21e6b9b9899527946d4c01ef9a0825cfad17
Chelsey <chelsey74@live.ca>
2022-02-05 18:20:21 -0700

    Fixed component render

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
 import React from 'react';
 import { GeolocatedProps, geolocated } from "react-geolocated";
 import './App.css';
+import WeatherComponent from './Components/WeatherComponent';
 import { fetchAddition } from './Api/ApiCall';
 
-function App() {
-
-  var c = fetchAddition("calgary", "C");
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  //const c = fetchAddition("calgary", "C");
+  render() {
+    return (
+      <WeatherComponent/>
+    );
+  }
 }
 
 export default geolocated()(App);
